a07a70cb9d353a069a3408da3061f4e3312089b2
lui-gi <70559474+lui-gi@users.noreply.github.com>
2025-11-27 20:45:09 -0500

    Fix router for GitHub pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// 1. Change this import from BrowserRouter to HashRouter
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import APlusPBQs from './pages/APlusPBQs.jsx'
@@ -9,15 +10,15 @@ import Layout from './Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    {/* 2. Change the component here too */}
+    <HashRouter>
       <Routes>
         <Route element={<Layout />}>
            <Route path="/" element={<App />} />
            <Route path="/a-plus-pbqs" element={<APlusPBQs />} />
-           {/* 2. Add the Route below */}
            <Route path="/security-plus-pbqs" element={<SecurityPlusPBQs />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
