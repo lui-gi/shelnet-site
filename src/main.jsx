24c135967a61c3d12160aabb35ec756891293937
Luigi <luigi.fern05@gmail.com>
2025-12-02 19:25:47 -0500

    reverted to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// 1. Change this import from BrowserRouter to HashRouter
-import { HashRouter, Routes, Route } from 'react-router-dom'
+// 1. CHANGE THIS IMPORT BACK
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import APlusPBQs from './pages/a-plus-pbqs.jsx'
@@ -10,8 +10,8 @@ import Layout from './Layout.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* 2. Change the component here too */}
-    <HashRouter>
+    {/* 2. USE BROWSER ROUTER HERE */}
+    <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
            <Route path="/" element={<App />} />
@@ -19,6 +19,6 @@ createRoot(document.getElementById('root')).render(
            <Route path="/security-plus-pbqs" element={<SecurityPlusPBQs />} />
         </Route>
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   </StrictMode>,
 )
