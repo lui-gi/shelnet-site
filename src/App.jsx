caf672f70e9ac0f920d9658bf2b4e3f080a8c266
luigi <luigi.fern05@gmail.com>
2026-06-19 15:45:01 -0400

    refactor: remove global BinaryRain canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import BinaryRain from './components/animations/BinaryRain';
 import HeroSection from './components/home/HeroSection';
 import PBQsSection from './components/home/PBQsSection';
 import ExamsSection from './components/home/ExamsSection';
@@ -11,11 +10,6 @@ import ConnectSection from './components/home/ConnectSection';
 export default function App() {
   return (
     <div className="min-h-screen bg-black text-white font-sans selection:bg-green-500/30 selection:text-green-200 relative">
-      {/* GLOBAL BINARY RAIN - Fixed position covering entire viewport */}
-      <div className="fixed inset-0 z-0 pointer-events-none">
-        <BinaryRain />
-      </div>
-
       {/* PAGE SECTIONS */}
       <HeroSection />
       <PBQsSection />
