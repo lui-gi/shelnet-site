50696d356ef1c569918119dbbef73eb25d36aa8c
luigi <luigi.fern05@gmail.com>
2026-06-24 19:50:43 -0400

    feat(modules): add guided-walkthrough runner and kinds map

---
+++ src/components/terminal/kinds/index.js
@@ -0,0 +1,6 @@
+// Maps a module's `kind` to its runner. Add new kinds here as they are built.
+import * as guidedWalkthrough from './guidedWalkthrough';
+
+export const RUNNERS = {
+  'guided-walkthrough': guidedWalkthrough,
+};
